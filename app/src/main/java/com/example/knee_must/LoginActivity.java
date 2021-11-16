22d9f91bfb5f977f0382cd502f05a91523ef0958
User <thd>
2021-11-16 09:32:17 +0200

    kuyfku

--- app/src/main/java/com/example/knee_must/LoginActivity.java
+++ app/src/main/java/com/example/knee_must/LoginActivity.java
@@ -2,13 +2,56 @@ package com.example.knee_must;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+EditText username;
+EditText password;
+Button btnlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        username=findViewById(R.id.etuname);
+        password=findViewById(R.id.etpassw);
+        btnlogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        boolean found = false;
+
+        for (int i=0; i<DataModel.doctors.size(); i++)
+        {
+            if (DataModel.doctors.get(i).getUsername().equals(username))
+            {
+                DataModel.doctor = DataModel.doctors.get(i);
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivityForResult(intent, 0);
+                found = true;
+            }
+        }
+        for (int i=0; i<DataModel.patients.size(); i++)
+        {
+            if (DataModel.patients.get(i).getUsername().equals(username))
+            {
+                DataModel.patient = DataModel.patients.get(i);
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivityForResult(intent, 0);
+                found = true;
+                
+            }
+        }
+
+        if (!found)
+        {
+            Toast.makeText(this, "User not found", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
